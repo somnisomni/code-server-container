5b568c517ee23e9793d159b9429848f07e8a0770
somni <me@somni.one>
2022-06-02 01:30:57 +0900

    Fix branding.sh for latest version

--- branding.sh
+++ branding.sh
@@ -4,4 +4,8 @@
 ### code-server custom branding script
 ### ... just for fun?
 
-sed -i 's/\"code-server\"/\"code-server-somni\"/g' /usr/local/bin/code-server/vendor/modules/code-oss-dev/product.json
+sed -i 's/\"nameShort\": \"code-server\"/\"nameShort\": \"code-server-somni\"/g' /usr/local/bin/code-server/lib/vscode/product.json
+sed -i 's/\"nameLong\": \"code-server\"/\"nameLong\": \"code-server-somni\"/g' /usr/local/bin/code-server/lib/vscode/product.json
+sed -i 's/\"applicationName\": \"code-server\"/\"applicationName\": \"code-server-somni\"/g' /usr/local/bin/code-server/lib/vscode/product.json
+sed -i 's/\"serverApplicationName\": \"code-server-oss\"/\"serverApplicationName\": \"code-server-somni\"/g' /usr/local/bin/code-server/lib/vscode/product.json
+
